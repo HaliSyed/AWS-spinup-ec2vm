6ec9dd124f649cf546d51cb87c6cc0619bdc8f8c
Noman Sadiq <noman.sadiq@osn.com>
2019-09-26 11:26:13 +0400

    all require file to spin up ec2 using terraform

---
+++ variables.tf
@@ -0,0 +1,20 @@
+
+
+
+
+variable "ami_name" {
+  type ="string"
+  description = "string"
+  default = "devopstesting"
+}
+variable "ami_id" {
+  type ="string"
+  description = "string"
+  default = "ami-04681a1dbd79675a5"
+
+}
+variable "ami_key_pair_name" {
+  type ="string"
+  description = "string"
+  default = "devopstest"
+}
